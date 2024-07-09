ec6ee0e1f255d23f2f966dad79f50c8fa9701abf
Jagan <kjagan1709@gmail.com>
2024-07-09 20:12:42 +0530

    second commit
    this is my second commit

--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const users=[{name:'Gugan'}]
+const users=[{name:'Jagan'}]
 router.get('/',(req,res) => {
     res.send('Users list')
 })
